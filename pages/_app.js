65ba7bba37b7786fd53f6af138c67eb67c6c2917
Tisha Di Fresco <tbdifresco@student.fullsail.edu>
2023-03-17 21:51:44 -0400

    Updating packages

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,33 @@
-import '@component/styles/globals.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import { SessionProvider } from "next-auth/react"
+import Navbar from '../components/Navbar'
+import '../styles/globals.css'
+import '../components/Navbar.css'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ 
+  Component, 
+  pageProps: session, ...pageProps },
+  ) {
+  return (
+    <SessionProvider session={session}>
+      <Navbar />
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
 }
+
+export default MyApp
+// import { Provider } from 'next-auth/react'
+// import Navbar from '../components/Navbar'
+// import '../styles/globals.css'
+// import '../components/Navbar.css'
+
+// function MyApp({ Component, pageProps }) {
+//   return (
+//     <Provider session={pageProps.session}>
+//       <Navbar />
+//       <Component {...pageProps} />
+//     </Provider>
+//   )
+// }
+
+// export default MyApp
